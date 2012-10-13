ec4d87f94387ddf5c2fe1e90f2475f19b25190ee
Дмитрий Бергштейн <bergstein@one2team.ru>
2012-10-14 01:02:02 +0400

    Protection from invalid package

--- src/Drivers/Package/List.php
+++ src/Drivers/Package/List.php
@@ -42,21 +42,24 @@
                             else
                                 $Private = '';*/
 
-                $Description = $Element['Meta']['Description'];
-                unset($Element['Meta']['Description']);
+                if (!empty($Element['Meta']['Package']))
+                {
+                    $Description = $Element['Meta']['Description'];
+                    unset($Element['Meta']['Description']);
 
-                $Package = 'Package: '.$Element['Meta']['Package'].PHP_EOL;
-                unset($Element['Meta']['Package']);
+                    $Package = 'Package: '.$Element['Meta']['Package'].PHP_EOL;
+                    unset($Element['Meta']['Package']);
 
-                $Element['Meta']['Size'] = filesize(Root.'/Data/Package/'.$Element['File']);
-                $Element['Meta']['MD5'] = md5_file(Root.'/Data/Package/'.$Element['File']);
-                $Element['Meta']['SHA1'] = sha1_file(Root.'/Data/Package/'.$Element['File']);
+                    $Element['Meta']['Size'] = filesize(Root.'/Data/Package/'.$Element['File']);
+                    $Element['Meta']['MD5'] = md5_file(Root.'/Data/Package/'.$Element['File']);
+                    $Element['Meta']['SHA1'] = sha1_file(Root.'/Data/Package/'.$Element['File']);
 
-                foreach ($Element['Meta'] as $Key => $Value)
-                    $Package.= $Key.': '.$Value.PHP_EOL;
+                    foreach ($Element['Meta'] as $Key => $Value)
+                        $Package.= $Key.': '.$Value.PHP_EOL;
 
-                $Package.= 'Filename: download/'.$Element['Name'].PHP_EOL;
-                $Output.= htmlspecialchars_decode($Package.'Description: '.$Description.PHP_EOL).PHP_EOL;
+                    $Package.= 'Filename: download/'.$Element['Name'].PHP_EOL;
+                    $Output.= htmlspecialchars_decode($Package.'Description: '.$Description.PHP_EOL).PHP_EOL;
+                }
             }
 
             file_put_contents($Call['PackagesFilename'], $Output);
